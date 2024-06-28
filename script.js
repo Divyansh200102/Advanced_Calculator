d0b03898393b4817feca48c6719862a1e4b08155
Divyansh200102 <divyanshkhatri200102@gmail.com>
2024-06-28 17:17:26 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,32 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    let display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    let display = document.getElementById('display');
+    display.value += operator;
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    let expression = display.value;
+
+    // Replace ^ with ** for exponentiation
+    expression = expression.replace(/\^/g, '**');
+
+    try {
+        display.value = eval(expression);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
